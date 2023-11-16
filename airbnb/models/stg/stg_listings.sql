8733ca6e27a826b257b8c09161d703465c5d02c2
Success Maharjan <successtorres9@gmail.com>
2023-11-16 15:18:48 +0545

    Added packages and stage model

---
+++ airbnb/models/stg/stg_listings.sql
@@ -0,0 +1,18 @@
+WITH source AS (
+        SELECT *
+
+        FROM {{ source('raw', 'raw_listings') }}
+)
+
+SELECT
+        ID as listing_id,
+        LISTING_URL,
+        NAME as listing_name,
+        ROOM_TYPE,
+        MINIMUM_NIGHTS,
+        HOST_ID,
+        PRICE as price_str,
+        CREATED_AT,
+        UPDATED_AT
+
+FROM source
